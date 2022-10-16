ad56e7049ead6c4ee2645876a05f3fe625ecceb1
Adam <melroseadam@gmail.com>
2022-10-15 23:54:54 -0400

    Figured out DOM

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+"use strict";
+
 console.log("Hello World!");
+
+let bgcBtn = document.getElementsByClassName("bgc-button");
+
+bgcBtn[0].addEventListener("click", function () {
+  bgcBtn[0].style.color = "black";
+});
+
+// document.querySelector('.check').addEventListener('click', function () {
+//     const guess = Number(document.querySelector('.guess').value);
+//     console.log(guess, typeof guess);
+
+// const color = prompt("Red or Blue?");
+// if (color === "Red") {
+//   document.getElementById(".first-paragraph").style.color = "red";
+// } else {
+//   document.getElementById(".first-paragraph").style.color = "blue";
+// }
+
+// let p1 = document.getElementsByClassName("first-paragraph");
+
+// p1 = "Is anything happening?";
