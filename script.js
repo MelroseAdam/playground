7fabc5dfbc5373c5126f824cc2613d923a36e5c4
Adam <melroseadam@gmail.com>
2022-10-17 23:11:25 -0400

    Added hompage with CSS

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 "use strict";
 
-let darkModeBtn = document.querySelector("#dark-mode");
-// const lightModeBtn = document.querySelector("#light-mode");
+const darkModeBtn = document.querySelector("#dark-mode");
+const menu = document.getElementById("menu");
 
-let turnOffLights = function () {
+Array.from(document.getElementsByClassName("menu-item")).forEach(
+  (item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    };
+  }
+);
+
+const turnOffLights = function () {
   document.body.classList.toggle("dark-mode");
   console.log("clicked");
 };
 
 darkModeBtn.addEventListener("click", turnOffLights);
 console.log(darkModeBtn);
-
-// darkModeBtn.onclick = turnOffLights;
